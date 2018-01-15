697d96ec202170eb825c9808c5210669194de04e
Julian Scharf <jscharf@thoughtworks.com>
2018-01-15 16:07:27 +1000

    Initial Commit

---
+++ jazzhands/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jazzhands
+//
+//  Created by Julian Scharf on 15/1/18.
+//  Copyright © 2018 Julian Scharf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
