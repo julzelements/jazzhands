2f40bd5c6bb83e08290a6e6685640c73efe0e022
Julian Scharf <jscharf@thoughtworks.com>
2018-01-16 11:31:58 +1000

    building the subtitle player

--- jazzhands/ViewController.swift
+++ jazzhands/ViewController.swift
@@ -10,21 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var subtitleOutput: UITextView!
+
+    @IBAction func loadSubs(_ sender: UIButton) {
+        loadSubtitleFile()
+    }
     var audioRecorder: AudioRecorder!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.audioRecorder = AudioRecorder()
+        subtitleOutput.backgroundColor = UIColor.blue
     }
     
     @IBAction func recordAudio(_ sender: Any) {
         print("started recording")
         self.audioRecorder.recordAudio()
+               subtitleOutput.backgroundColor = UIColor.yellow
     }
 
     @IBAction func stopRecording(_ sender: Any) {
         print("stopped recording")
         self.audioRecorder.stopRecording()
+               subtitleOutput.backgroundColor = UIColor.green
+    }
+    
+    func loadSubtitleFile() {
+        let rawString = SubtitleIO.getRawStringFromFileInBundle(fileName: "spiderman", fileExtension: "srt")
+        
+        let myEvents = SubtitleEvents(rawSRTString: rawString)
+        
+        let systemTime = MyTime()
+        
+        var player = Player(apiSystemTime: 12, apiMovieTime: 12, arrayOfEvents: myEvents, time: systemTime)
+    
+
     }
     
     
@@ -33,6 +53,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
 
